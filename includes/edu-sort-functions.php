95449859d7c257107570c1f373b9b9e12d9f014b
Chris Gårdenberg <chga@multinet.se>
2018-08-14 15:56:41 +0200

    - chg: Adding more checks to multiSort to get rid of notices/warnings

--- includes/edu-sort-functions.php
+++ includes/edu-sort-functions.php
@@ -3,15 +3,21 @@
 function sortEvents( $events, $order_by, $order ) {
 	usort( $events, function( $evA, $evB ) use ( $order_by, $order ) {
 		return multiSort( $evA, $evB, $order_by, $order, 0 );
-	});
+	} );
+
 	return $events;
 }
 
 function multiSort( $evA, $evB, $order_by, $order, $idx ) {
-	if ( $evA[$order_by[$idx]] === $evB[$order_by[$idx]] && sizeof( $order_by ) > $idx ) {
+	if ( sizeof( $order_by ) > $idx && $evA[ $order_by[ $idx ] ] === $evB[ $order_by[ $idx ] ] ) {
 		$idx += 1;
+
 		return multiSort( $evA, $evB, $order_by, $order, $idx );
 	}
 
-	return ( strcmp( $evA[$order_by[$idx]], $evB[$order_by[$idx]] ) ) * ( $order[$idx] !== null ? $order[$idx] : 1 );
+	if ( isset( $order_by[ $idx ] ) ) {
+		return @( strcmp( $evA[ $order_by[ $idx ] ], $evB[ $order_by[ $idx ] ] ) ) * ( $order[ $idx ] !== null ? $order[ $idx ] : 1 );
+	}
+
+	return false;
 }
