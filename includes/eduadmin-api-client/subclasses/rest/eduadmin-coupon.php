c329e2b4c32b4adfb103ed398036249f039990af
Chris Gårdenberg <chga@multinet.se>
2018-11-14 12:49:29 +0100

    - updated eduadmin-api-client with new version

--- includes/eduadmin-api-client/subclasses/rest/eduadmin-coupon.php
+++ includes/eduadmin-api-client/subclasses/rest/eduadmin-coupon.php
@@ -22,4 +22,21 @@ class EduAdmin_REST_Coupon extends EduAdminRESTClient {
 			get_called_class() . '|' . __FUNCTION__
 		);
 	}
+
+	/**
+	 * @param integer $programme_start_id
+	 * @param string $coupon_code
+	 *
+	 * @return bool|mixed
+	 */
+	public function ProgrammeStartCouponIsValid( $programme_start_id, $coupon_code ) {
+		return parent::GET(
+			'/ProgrammeStart/IsValid',
+			array(
+				'programmeStartId' => $programme_start_id,
+				'couponCode' 	   => $coupon_code,
+			),
+			get_called_class() . '|' . __FUNCTION__
+		);
+	}
 }
