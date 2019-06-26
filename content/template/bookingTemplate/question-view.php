3febc13af025e61d2e589f6c29a2023693a6ec05
Chris Gårdenberg <chga@multinet.se>
2019-06-26 16:53:01 +0200

    fix: 🐛 Show questions regardless of eventid in query

--- content/template/bookingTemplate/question-view.php
+++ content/template/bookingTemplate/question-view.php
@@ -1,9 +1,7 @@
 <div class="questionPanel">
 	<?php
-	if ( ! empty( $_REQUEST['eid'] ) ) {
-		foreach ( $booking_questions as $question ) {
-			render_question( $question, false, 'booking' );
-		}
+	foreach ( $booking_questions as $question ) {
+		render_question( $question, false, 'booking' );
 	}
 	?>
 </div>
