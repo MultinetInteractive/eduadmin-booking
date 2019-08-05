302387d2b98a56ef036b6c24147bfd23cf306c66
Chris Gårdenberg <chga@multinet.se>
2019-08-05 10:14:50 +0200

    refactor: 💡 Updated the is_checked method to support inputs

--- src/eduadmin.php
+++ src/eduadmin.php
@@ -144,7 +144,7 @@ if ( ! class_exists( 'EduAdmin' ) ) :
 			}
 
 			if ( $as_json ) {
-				echo '<xmp>' . json_encode( $object, JSON_PRETTY_PRINT ) . '</xmp>';
+				echo '<span style="white-space: pre;">' . json_encode( $object, JSON_PRETTY_PRINT ) . '</span>';
 
 				return;
 			}
@@ -152,14 +152,39 @@ if ( ! class_exists( 'EduAdmin' ) ) :
 			ob_start();
 			var_dump( $object );
 
-			echo '<xmp>' . ob_get_clean() . '</xmp>';
+			echo '<span style="white-space: pre;">' . ob_get_clean() . '</span>';
 		}
 
-		public function is_checked( $optionValue ) {
-			return ! empty( $optionValue );
+		/*
+		 * Checks if the current option is checked inside
+		 * @param string|bool|number|null $optionValue
+		 * @param bool|null $default
+		 * @return bool
+		 */
+		public function is_checked( $optionValue, $default = false ) {
+			if ( empty( $optionValue ) ) {
+				return $default;
+			}
+			if ( 'on' === $optionValue ) {
+				return true;
+			}
+			if ( true === $optionValue ) {
+				return true;
+			}
+			if ( 'true' === $optionValue ) {
+				return true;
+			}
+			if ( 1 === $optionValue ) {
+				return true;
+			}
+			if ( '1' === $optionValue ) {
+				return true;
+			}
+
+			return $default;
 		}
 
-		public function is_selected( $optionValue, $currentValue ) {
+		public function is_selected( $optionValue, $currentValue, $defaultValue ) {
 			return ! empty( $optionValue ) && $optionValue === $currentValue;
 		}
 
