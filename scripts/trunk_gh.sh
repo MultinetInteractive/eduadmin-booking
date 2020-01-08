7bb482a666a0751ba4f1fef695df7e79b1b3e342
Chris Gårdenberg <chga@multinet.se>
2020-01-08 13:09:03 +0100

    chore: Removing more files not needed for deployment

--- scripts/trunk_gh.sh
+++ scripts/trunk_gh.sh
@@ -23,6 +23,8 @@ echo "Version: $VERSION of $PLUGIN"
 
 # Remove files not needed in plugin for deployment
 rm -f $PROJECT_ROOT/composer.json
+rm -f $PROJECT_ROOT/commitlint.config.js
+rm -f $PROJECT_ROOT/tsconfig.json
 rm -f $PROJECT_ROOT/.scrutinizer.yml
 rm -f $PROJECT_ROOT/CONTRIBUTING.md
 rm -f $PROJECT_ROOT/LICENSE.md
