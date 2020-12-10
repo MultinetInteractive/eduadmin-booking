3dbd5e75d2a7850a8812b48ffed5ba5e54da5ec0
Chris Gårdenberg <chga@multinet.se>
2020-12-10 13:33:02 +0100

    fix: Move robot-check into the other checks first, to not block creation of users in WP

--- class/class-eduadmin-bookinghandler.php
+++ class/class-eduadmin-bookinghandler.php
@@ -58,12 +58,12 @@ class EduAdmin_BookingHandler {
 	}
 
 	public function process_booking() {
-		if ( ! empty( $_POST['username'] ) || ! empty( $_POST['email'] ) ) {
-			wp_redirect( get_page_link( '/' ) );
-			exit( 0 );
-		}
-
 		if ( ! empty( $_POST['edu-valid-form'] ) && wp_verify_nonce( $_POST['edu-valid-form'], 'edu-booking-confirm' ) && ! empty( $_POST['act'] ) && 'bookCourse' === sanitize_text_field( $_POST['act'] ) ) { // Var input okay.
+			if ( ! empty( $_POST['username'] ) || ! empty( $_POST['email'] ) ) {
+				wp_redirect( get_page_link( '/' ) );
+				exit( 0 );
+			}
+
 			$single_person_booking = EDU()->is_checked( 'eduadmin-singlePersonBooking', false );
 
 			if ( ! $this->verify_recaptcha() ) {
