17bdc936fc50ac6ee2b3b9682cae21b448ee1b3f
Chris Gårdenberg <chga@multinet.se>
2022-02-21 14:31:12 +0100

    ci(Website): Update config for website with trailing slash, to fix warnings for GitHub Pages

--- new_website/docusaurus.config.js
+++ new_website/docusaurus.config.js
@@ -13,6 +13,7 @@ module.exports = {
 		"users": [],
 		"repoUrl": "https://github.com/MultinetInteractive/EduAdmin-WordPress"
 	},
+	"trailingSlash": true,
 	"onBrokenLinks": "log",
 	"onBrokenMarkdownLinks": "log",
 	"presets": [
